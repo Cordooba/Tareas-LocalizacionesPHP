729f0000a20194b26fa5c55454550e2964cf3b85
Cordooba <cordooba_92@hotmail.es>
2015-11-14 21:08:24 +0100

    Maps

--- maps/index.php
+++ maps/index.php
@@ -1,18 +1,24 @@
 <?php 
 
-require_once (dirname(dirname(__FILE__))).'app/info.php';
-require_once (__ROOT__.'db/connectdb.php');
+require_once '.././app/info.php';
+require_once '.././db/connectdb.php';
 
 try {
+	
+	$sql = 'SELECT task, level, idLocation, location, latitud, longitud FROM location JOIN tasks ON location.idLocation = tasks.idLocal;';
+	$ps = $pdo->prepare($sql);
+	$ps->execute();
 
-	
-	
 } catch (PDOException $e) {
-	
-	echo "".$e->getMessage();
+
+	die("No se ha podido extraer información de la base de datos:". $e->getMessage());
 
 }
+while ( $row = $ps->fetch(PDO::FETCH_ASSOC) ) {
 
+	$locality[] = $row;
+
+}
 
 require_once 'map.html.php';
 
--- maps/map.html.php
+++ maps/map.html.php
@@ -19,24 +19,80 @@
     <div id="map"></div>
     <script>
 
-function initMap() {
+  function initMap() {
+
   var myLatLng = {lat: 37.4105634, lng: -5.9251314};
 
-  
+  <?php 
 
-  var myLatLngADA = {lat: 37.4078108, lng: -5.9282535};
+  if ( !empty($locality) ) {
+
+    foreach ($locality as $local) {
+    
+      $place = "var myLatLng".$local['idLocation']." = { lat: ".$local['latitud'].", lng:".$local['longitud']."}; \n  ";
+
+      echo $place;
+
+    }
+
+  }
+
+  ?>
+
+  //var myLatLngADA = {lat: 37.4078108, lng: -5.9282535};
 
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
     center: myLatLng
   });
 
+  <?php foreach ($locality as $local) : ?>
+
+  //var contentString = '<div id="content">'+
+  //    '<div id="siteNotice">'+
+  //    '</div>'+
+  //    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+  //    '<div id="bodyContent">'+
+  //    '<p><b>Uluru</b>, </p>'+
+  //    '</div>'+
+  //    '</div>';    
+
+  //var infowindow = new google.maps.InfoWindow({
+  //  content: contentString<?=$local['idLocation']?>
+  //});
+
   var marker = new google.maps.Marker({
-    position: myLatLngADA,
+    position: myLatLng<?=$local['idLocation']?>,
+    /*icon: <?php switch ( $local['level'] ) {
+          case '1':
+            echo "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png";
+            break;
+          case '2':
+            echo "http://maps.google.com/mapfiles/ms/icons/green-dot.png";
+            break;
+          case '3':
+            echo "http://maps.google.com/mapfiles/ms/icons/purple-dot.png";
+            break;
+          case '4':
+            echo "http://maps.google.com/mapfiles/ms/icons/blue-dot.png";
+            break;
+          case '5':
+            echo "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
+            break;
+          default:
+          break;
+    }
+    ?>, */
     map: map,
-    title: 'ADA ITS'
+    title: '<?=$local['location']?>'
   });
-}
+
+  //marker.addListener('click', function() {
+  //  infowindow.open(map, marker);
+  //});
+
+  <?php endforeach; ?>
+  }
 
     </script>
     <script async defer
